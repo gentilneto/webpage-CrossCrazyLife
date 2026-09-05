7882b7e116576b7debb4c63492adca6da8cb7853
gentilneto <gentilc.neto@gmail.com>
2026-09-05 16:31:40 -0300

    chore: initial setup and project configuration

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Trails from './components/Trails/Trails';
+import Racetrack from './components/Racetrack/Racetrack';
+import Rental from './components/Rental/Rental';
+import Paddock from './components/Paddock/Paddock';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Trails />
+      <Racetrack />
+      <Rental />
+      <Paddock />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
